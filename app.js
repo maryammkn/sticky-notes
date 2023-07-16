52289ec4090ee941e907fcb172ba3456f4b03a38
morteza <mm.gov.1381@gmail.com>
2023-07-16 18:40:41 +0330

    save notes in local storage

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const notesContainer = document.querySelector("#app");
 const addNoteBtn = document.querySelector(".add-note");
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("notes") || []);
+}
+
+function saveNotes(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
